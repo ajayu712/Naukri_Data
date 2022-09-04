13df0d6b25bebc258bfabc8b7918182fcf26819e
Ajay Upadhyaya <ajayu712@gmail.com>
2022-09-05 01:28:31 +0530

    Update indeed_scraper.py
    Added Extracted Data to Pandas DF

--- Data_Scraping/indeed_scraper.py
+++ Data_Scraping/indeed_scraper.py
@@ -21,21 +21,23 @@ def extract(page):
     return soupData
 
 def transform(soupData):
-    df =pd.DataFrame(columns=['Title','Company','Ratings','Reviews','URL','Experience','Salary','Location','Skills Required'])
+    df = pd.DataFrame(columns=['Title','Company','Ratings','Reviews','URL','Experience','Salary','Location','Skills Required'])
     results = soupData.find(class_='list')
+
     job_details = results.find_all('article',class_='jobTuple bgWhite br4 mb-8')
+
     for jobs in job_details:
         # Extracting Job Titles
         title = jobs.find('a',class_='title fw500 ellipsis')
-        print(title.text)
+        # print(title.text)
 
         # Extracting Job URL
         url = jobs.find('a',class_='title fw500 ellipsis').get('href')
-        print(url)
+        # print(url)
 
         # Extracting Company Name
         company = jobs.find('a',class_='subTitle ellipsis fleft')
-        print(company.text)
+        # print(company.text)
 
         # Extracting Job Ratings
         rating_span = jobs.find('span',class_='starRating fleft dot')
@@ -43,7 +45,7 @@ def transform(soupData):
             continue
         else:
             ratings = rating_span.text
-            print(ratings)
+            # print(ratings)
 
         # Extracting Job Reviews
         review_a = jobs.find('a',class_='reviewsCount ml-5 fleft blue-text')
@@ -51,27 +53,29 @@ def transform(soupData):
             continue
         else:
             reviews = review_a.text
-            print(reviews)
+            # print(reviews)
 
         # Extracting Experience Required
         exp_req = jobs.find(class_='fleft grey-text br2 placeHolderLi experience').find('span')
-        print(exp_req.text)
+        # print(exp_req.text)
 
         # Extracting Job Salary        
         salary = jobs.find(class_='fleft grey-text br2 placeHolderLi salary').find('span')
-        print(salary.text)
+        # print(salary.text)
 
         # Extracting Job Location
         location = jobs.find(class_='fleft grey-text br2 placeHolderLi location').find('span')
-        print(location.text)
+        # print(location.text)
 
         # Extracting Skills Required
         skills_list=[]
         for job_skills in jobs.find('ul',class_='tags has-description'):
             skills_list.append(job_skills.text)
-        print(skills_list)
+        # print(skills_list)
 
-        print(" "*2)
+        # print(" "*2)
+        df = df.append({'URL':url,'Title':title.text,'Company':company.text,'Ratings':ratings,'Reviews':reviews,'Experience':exp_req.text,'Salary':salary.text,'Location':location.text,'Skills Required':skills_list},ignore_index=True)
+    print(df.head())
 
 val = extract(2)
 transform(val)
